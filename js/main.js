f0a680c697569a810dc76284d1d6c5e21d962104
AzizRakhim <azizbekrahimjonov80@gmail.com>
2022-01-18 23:59:25 +0500

    added modal

--- js/main.js
+++ js/main.js
@@ -1,3 +1,15 @@
 document.getElementById('modeBtn').addEventListener('click', function() {
     document.body.classList.toggle('dark');
 })
+
+let elOpenModalBtn = document.getElementById('searchBtn');
+let elCloseModalBtn = document.getElementById('closeModal');
+let elModalBox = document.getElementById('modalBox');
+
+elOpenModalBtn.addEventListener('click', function() {
+    elModalBox.classList.add('show');
+})
+
+elCloseModalBtn.addEventListener('click', function() {
+    elModalBox.classList.remove('show');
+})
